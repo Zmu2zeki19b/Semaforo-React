8ce282cb41b702c9e62c3d7433be6c2e2e7d18e9
Ezequiel Blanco <ezequielgier19@gmail.com>
2025-02-03 19:19:29 +0000

    Semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react"; 
+import "../../styles/home.css";  
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+const TrafficLight = () => {
+	const [color, setColor] = useState("red");
+	const [extraColor, setExtraColor] = useState(false);
+	const handleClick = (selectedColor) => {
+	  if (selectedColor === "purple" && extraColor) {
+		setColor("purple");  
+	  } else if (selectedColor !== "purple") {
+		setColor(selectedColor); 
+	  }
+	};
+	const toggleColor = () => {
+	  const colors = extraColor ? ["red", "yellow", "green", "purple"] : ["red", "yellow", "green"];
+	  const nextColor = colors[(colors.indexOf(color) + 1) % colors.length];
+	  setColor(nextColor);
+	};
+	const addPurple = () => {
+	  setExtraColor(!extraColor); 
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div className="traffic-light">
+		<div
+		  className={`light ${color === "red" ? "active" : ""}`}
+		  id="red"
+		  onClick={() => handleClick("red")}
+		></div>
+		<div
+		  className={`light ${color === "yellow" ? "active" : ""}`}
+		  id="yellow"
+		  onClick={() => handleClick("yellow")}
+		></div>
+		<div
+		  className={`light ${color === "green" ? "active" : ""}`}
+		  id="green"
+		  onClick={() => handleClick("green")}
+		></div>
+		{extraColor && (
+		  <div
+			className={`light ${color === "purple" ? "active" : ""}`}
+			id="purple"
+			onClick={() => handleClick("purple")}
+		  ></div>
+		)}
+		<button onClick={toggleColor}>Alternar Color</button>
+		<button onClick={addPurple}>Añadir color púrpura</button>
+	  </div>
 	);
+  };
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Semáforo Interactivo</h1>
+      <TrafficLight />
+    </div>
+  );
 };
-
 export default Home;
